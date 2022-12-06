65af837b1a3d3f8bd3907b60d4676b65809468f8
Thorfinn <402832626@qq.com>
2022-12-06 22:30:50 +0800

    go struct

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"go/basic/example/package1"
-)
+import "go/basic/example/struct_func"
 
 // import "go/basic/hello"
 
 func main() {
 	// exercise.Season(7)
-	package1.UpperCase("life's little bit messy")
+	struct_func.Rec()
 }
